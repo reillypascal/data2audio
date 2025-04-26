e3a5b78260e1c8439f664d534a7fe9dcc0f0d636
Reilly Spitzfaden <reillypascal@gmail.com>
2025-04-26 15:26:11 -0400

    walks thru input dir, outputs to output/

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,73 @@
+// use std::{env, fs};
 use std::fs;
+use std::path::{self, Path, PathBuf, StripPrefixError}; 
 use hound;
+use walkdir::{DirEntry, WalkDir};
 
 fn main() {
-    let data: Vec<u8> = fs::read("input/OFXLoader").expect("Error");
+    // let current_dir = env::current_dir().expect("Error getting directory");
+    // read dir
+    WalkDir::new("input/")
+        .into_iter()
+        .filter_map(|entry| entry.ok())
+        .for_each(|entry| {
+            if let Ok(metadata) = entry.metadata() {
+                if metadata.is_file() {
+                    // import file as Vec<u8>
+                    let data: Vec<u8> = fs::read(entry.path()).expect("Error reading file");
+                    
+                    // convert to Vec<i16>
+                    let mut converted_data: Vec<i16> = Vec::new();
+                    
+                    let iter = data.chunks_exact(2);
+                    for item in iter {
+                        converted_data.push(i16::from_le_bytes(item.try_into().unwrap()));
+                    }
+                    
+                    // write all files into output directory
+                    let mut write_path = PathBuf::from("output/");
+                    
+                    // entry.path().file_name() returns an Option
+                    if let Some(file_name) = entry.path().file_name() {
+                        write_path.push(file_name);
+                        write_path.set_extension("wav");
+                        write_file_as_wav(converted_data, write_path);
+                    }
+                    
+                    
+                    // if let Ok(mut write_path) = replace_prefix(entry.path().to_path_buf(), "input/", "output/") {
+                    //     let prefix = write_path.parent().expect("Could not get parent");
+                    //     fs::create_dir_all(prefix).expect("Could not create dir");
+                        
+                    //     write_path.set_extension("wav");
+                        
+                    //     write_file_as_wav(converted_data, write_path);
+                    // }
+                }
+            }
+        });
     
-    let mut converted_data: Vec<i16> = Vec::new();
+    // // import file as Vec<u8>
+    // let data: Vec<u8> = fs::read("input/OFXLoader").expect("Error reading file");
     
-    let iter = data.chunks_exact(2);
+    // // convert to Vec<i16>
+    // let mut converted_data: Vec<i16> = Vec::new();
     
-    for item in iter {
-        converted_data.push(i16::from_le_bytes(item.try_into().unwrap()));
-    }
+    // let iter = data.chunks_exact(2);
+    // for item in iter {
+    //     converted_data.push(i16::from_le_bytes(item.try_into().unwrap()));
+    // }
     
+    // write_file_as_wav(converted_data, );
+}
+
+// fn replace_prefix(p: impl AsRef<Path>, from: impl AsRef<Path>, to: impl AsRef<Path>) -> Result<PathBuf, StripPrefixError> {
+//   p.as_ref().strip_prefix(from).map(|p| to.as_ref().join(p))
+// }
+
+fn write_file_as_wav(data: Vec<i16>, name: path::PathBuf) {
+    // write WAV file
+    // spec
     let spec = hound::WavSpec {
         channels: 1,
         sample_rate: 44100,
@@ -19,14 +75,10 @@ fn main() {
         sample_format: hound::SampleFormat::Int,
     };
     
-    let mut writer = hound::WavWriter::create("output/OFXLoader.wav", spec).expect("Could not create writer");
-    
-    for t in 0..converted_data.len() {
-        writer.write_sample(converted_data[t]).expect("Could not write sample");
+    // writer
+    let mut writer = hound::WavWriter::create(name, spec).expect("Could not create writer");
+    for t in 0..data.len() {
+        writer.write_sample(data[t]).expect("Could not write sample");
     }
-    
     writer.finalize().expect("Could not finalize WAV file");
-    
-    // let s = format!("{:?}", &data);
-    // println!("{}", s);
 }
