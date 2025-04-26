570aaf800619b58930a813a6d2f52c4b7d15233f
Reilly Spitzfaden <reillypascal@gmail.com>
2025-04-26 12:21:49 -0400

    convert bytes to i16 for wav

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,14 @@ use hound;
 fn main() {
     let data: Vec<u8> = fs::read("input/OFXLoader").expect("Error");
     
+    let mut converted_data: Vec<i16> = Vec::new();
+    
+    let iter = data.chunks_exact(2);
+    
+    for item in iter {
+        converted_data.push(i16::from_le_bytes(item.try_into().unwrap()));
+    }
+    
     let spec = hound::WavSpec {
         channels: 1,
         sample_rate: 44100,
@@ -13,8 +21,8 @@ fn main() {
     
     let mut writer = hound::WavWriter::create("output/OFXLoader.wav", spec).expect("Could not create writer");
     
-    for t in 0..data.len() {
-        writer.write_sample(data[t] as i16).expect("Could not write sample");
+    for t in 0..converted_data.len() {
+        writer.write_sample(converted_data[t]).expect("Could not write sample");
     }
     
     writer.finalize().expect("Could not finalize WAV file");
